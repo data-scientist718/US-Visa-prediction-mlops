865a383aa37e5ce37f6e2a757fd301822b6f390d
abdurrehman <abdirrehman.bse@gmail.com>
2024-07-25 09:26:09 +0200

    requirements done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="ABD_UR_REHMAN",
+    author_email="abdurrehman.bse@gmail.com",
+    packages=find_packages()
+)
