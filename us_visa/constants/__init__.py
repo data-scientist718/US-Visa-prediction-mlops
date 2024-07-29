74b68322255dd7d419322b988dc232b463987979
abdurrehman <abdirrehman.bse@gmail.com>
2024-07-29 13:52:10 +0200

    data validation added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -16,6 +16,11 @@ TEST_FILE_NAME: str = "test.csv"
 FILE_NAME: str = "usvisa.csv"
 MODEL_FILE_NAME = "model.pkl"
 
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = date.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
@@ -24,3 +29,11 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
--- us_visa/pipline/training_pipeline.py
+++ us_visa/pipline/training_pipeline.py
@@ -2,18 +2,18 @@ import sys
 from us_visa.exception import USvisaException
 from us_visa.logger import logging
 from us_visa.components.data_ingestion import DataIngestion
-# from us_visa.components.data_validation import DataValidation
+from us_visa.components.data_validation import DataValidation
 # from us_visa.components.data_transformation import DataTransformation
 # from us_visa.components.model_trainer import ModelTrainer
 
 
-from us_visa.entity.config_entity import DataIngestionConfig
-                                        #  DataValidationConfig,
+from us_visa.entity.config_entity import (DataIngestionConfig,
+                                        DataValidationConfig)
                                         #  DataTransformationConfig,
                                         #  ModelTrainerConfig)
 
-from us_visa.entity.artifact_entity import DataIngestionArtifact
-                                            # DataValidationArtifact,
+from us_visa.entity.artifact_entity import (DataIngestionArtifact,
+                                            DataValidationArtifact)
                                             # DataTransformationArtifact,
                                             # ModelTrainerArtifact)
 
@@ -21,7 +21,7 @@ from us_visa.entity.artifact_entity import DataIngestionArtifact
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
-#       self.data_validation_config = DataValidationConfig()
+        self.data_validation_config = DataValidationConfig()
 #        self.data_transformation_config = DataTransformationConfig()
 #        self.model_trainer_config = ModelTrainerConfig()
 
@@ -46,29 +46,29 @@ class TrainPipeline:
         
 
     
-    # def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
-    #     """
-    #     This method of TrainPipeline class is responsible for starting data validation component
-    #     """
-    #     logging.info("Entered the start_data_validation method of TrainPipeline class")
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
 
-    #     try:
-    #         data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
-    #                                          data_validation_config=self.data_validation_config
-    #                                          )
+        try:
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=self.data_validation_config
+                                             )
 
-    #         data_validation_artifact = data_validation.initiate_data_validation()
+            data_validation_artifact = data_validation.initiate_data_validation()
 
-    #         logging.info("Performed the data validation operation")
+            logging.info("Performed the data validation operation")
 
-    #         logging.info(
-    #             "Exited the start_data_validation method of TrainPipeline class"
-    #         )
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
 
-    #         return data_validation_artifact
+            return data_validation_artifact
 
-    #     except Exception as e:
-    #         raise USvisaException(e, sys) from e
+        except Exception as e:
+            raise USvisaException(e, sys) from e
         
 
 
@@ -110,7 +110,7 @@ class TrainPipeline:
         """
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            # data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             # data_transformation_artifact = self.start_data_transformation(
             #     data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
             # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
